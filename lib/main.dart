0c87ef53a801283f0c2c4e19f132b24e1a1b6a50
Ivan Sokarev <sokarev.ivan@mail.ru>
2023-11-14 20:19:56 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fitness',
+      theme: ThemeData(
+        // textTheme:
+        //     TextTheme(bodyText1: TextStyle(color: ColorConstants.textColor)),
+        // fontFamily: 'NotoSansKR',
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // home: isLoggedIn ? TabBarPage() : OnboardingPage(),
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+      ),
+      body: const Column(
+        children: [
+          Text('Hello world!'),
+        ],
+      ),
+    );
+  }
+}
